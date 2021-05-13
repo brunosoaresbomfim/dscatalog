e057449f114e2e9421e4bf069fa4ee5794585b78
Bruno Bomfim <bs01bs@gmail.com>
2021-05-13 14:39:09 -0300

    Post new category

--- backend/src/main/java/br/edu/fpo/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/br/edu/fpo/dscatalog/resources/CategoryResource.java
@@ -1,13 +1,17 @@
 package br.edu.fpo.dscatalog.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.edu.fpo.dscatalog.dto.CategoryDTO;
 import br.edu.fpo.dscatalog.services.CategoryService;
@@ -18,16 +22,23 @@ public class CategoryResource {
 
 	@Autowired
 	private CategoryService service;
-	
+
 	@GetMapping
-	public ResponseEntity<List<CategoryDTO>> findAll(){
+	public ResponseEntity<List<CategoryDTO>> findAll() {
 		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping("/{id}")
-	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id){
+	public ResponseEntity<CategoryDTO> findById(@PathVariable Long id) {
 		CategoryDTO categoryFound = service.findById(id);
 		return ResponseEntity.ok().body(categoryFound);
+	}
+
+	@PostMapping
+	public ResponseEntity<CategoryDTO> insert(@RequestBody CategoryDTO dto) {
+		dto = service.save(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 }
