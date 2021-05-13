f45127887788ad9b2bab9a70b213f2993b02cedd
Bruno Bomfim <bs01bs@gmail.com>
2021-05-13 14:16:47 -0300

    Created exceptions of resource and services, Find Category by id

--- backend/src/main/java/br/edu/fpo/dscatalog/services/CategoryService.java
+++ backend/src/main/java/br/edu/fpo/dscatalog/services/CategoryService.java
@@ -1,6 +1,7 @@
 package br.edu.fpo.dscatalog.services;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import br.edu.fpo.dscatalog.dto.CategoryDTO;
+import br.edu.fpo.dscatalog.entities.Category;
 import br.edu.fpo.dscatalog.repositories.CategoryRepository;
+import br.edu.fpo.dscatalog.services.exceptions.EntityNotFoundException;
 
 @Service
 public class CategoryService {
@@ -18,14 +21,13 @@ public class CategoryService {
 	
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll(){
-//		List<Category> list = repository.findAll();
-//		
-//		List<CategoryDTO> listDTO = new ArrayList<>();
-//		for (Category category : list) {
-//			listDTO.add(new CategoryDTO(category));
-//		}
-//		
-//		return listDTO;
 		return repository.findAll().stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());				
+	}
+	
+	@Transactional(readOnly = true)
+	public CategoryDTO findById(Long id) {
+		Optional<Category> category = repository.findById(id);
+		Category entity = category.orElseThrow(() -> new EntityNotFoundException("Category not found!"));
+		return new CategoryDTO(entity);
 	}
 }
